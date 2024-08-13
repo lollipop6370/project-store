0034eb9e7d895af49d0723bc4a4dc5f1cc00506d
lollipop6370 <knn101do@gmail.com>
2024-08-13 12:54:21 +0800

    ok

--- store_vue/src/main.js
+++ store_vue/src/main.js
@@ -1,10 +1,13 @@
+import './assets/styles.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
---
+++ store_vue/src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/product/:id',
+    name: 'product-details',
+    // 這裡的懶加載有助於優化應用性能
+    component: () => import('../views/ProductDetailsView.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: () => import('../views/CartView.vue')
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: () => import('../views/CheckoutView.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: () => import('../views/UserProfileView.vue')
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
