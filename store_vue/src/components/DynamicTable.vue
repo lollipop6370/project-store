e8854bda026f872145b576fc4e53f17fd83344c0
lollipop6370 <knn101do@gmail.com>
2024-08-25 20:12:53 +0800

    sub

---
+++ store_vue/src/components/DynamicTable.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(row, rowIndex) in data" :key="rowIndex">
+            <td v-for="(cell, cellIndex) in row" :key="cellIndex">{{ cell }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DynamicTable',
+    props: {
+      data: {
+        type: Array,
+        required: true
+      },
+      headers: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+  }
+  </style>
+  
