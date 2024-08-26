53705387d38381b35cb17c0490e603a9ef18a457
lollipop6370 <knn101do@gmail.com>
2024-08-26 23:36:03 +0800

    1

--- store_vue/src/page/backend/BEHome.vue
+++ store_vue/src/page/backend/BEHome.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>hi</div>
+    <div class="welcome">歡迎光臨商城後臺管理系統</div>
 </template>
 
 <script setup>
 </script>
 
 <style scoped>
+.welcome {
+    display: flex;
+    font-size: 60px;
+    position: absolute;
+    left: 400px;
+    top: 200px;
+}
 </style>
